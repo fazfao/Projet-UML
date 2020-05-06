7d95da46f3155a678144a24ca7aeea1899f1a804
fazfao <zihao23333@gmail.com>
2020-05-06 16:14:20 +0200

    mise à jour de classe Utilisateurs

--- Utilisateurs.cpp
+++ Utilisateurs.cpp
@@ -2,6 +2,16 @@ using namespace std;
 
 #include "Utilisateurs.h"
 
+bool Utilisateurs::Authentification(const string mel, const string mdp) const
+{
+  return (mel==mail)&&(mdp==password);
+}
+
+bool Utilisateurs::LogOut() const 
+{
+  cout<<"Vous avec déconnecté";
+}
+
 Utilisateurs::Utilisateurs ( const Utilisateurs & unUtilisateurs )
 {
 #ifdef MAP
@@ -15,7 +25,13 @@ Utilisateurs::Utilisateurs()
 #ifdef MAP
     cout << "Appel au constructeur de <Utilisateurs>" << endl;
 #endif
-} 
+}
+ 
+Utilisateurs::Utilisateurs(const string mail, const string password)
+{
+  this.mail=mail;
+  this.password=password;
+}
 
 Utilisateurs::~Utilisateurs ( )
 {
@@ -23,3 +39,4 @@ Utilisateurs::~Utilisateurs ( )
     cout << "Appel au destructeur de <Utilisateurs>" << endl;
 #endif
 } 
+
--- Utilisateurs.h
+++ Utilisateurs.h
@@ -7,13 +7,18 @@ class Utilisateurs
 {
 
 public:
+    
+    virtual bool Authentification(const string mel, const string mdp) const;
+    virtual bool LogOut() const;
 
     Utilisateurs ( const Utilisateurs & unUtilisateurs );
-
     Utilisateurs();
+    Utilisateurs(const string mail, const string password);
+    virtual ~User();
 
 protected:
-
+    string mail;
+    string password;
 };
 
 #endif // UTILISATEURS_H
